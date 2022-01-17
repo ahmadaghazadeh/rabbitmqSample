44cfe03acc434f1d8daae453579342d81202819c
Ahmad Aghazadeh <890683@DBI.COM>
2022-01-17 09:17:08 +0330

    First commit

---
+++ Receive/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "192.168.0.116",
+    UserName = "yetir",
+    Password = "asdf1234",
+    Port = 5672,
+    VirtualHost = "/"
+};
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Yetir.DistributionFinalize",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] Received {0}", message);
+    };
+    channel.BasicConsume(queue: "Yetir.DistributionFinalize",
+                         autoAck: true,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ rabbitmqHelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { 
+    HostName = "192.168.0.116",
+    UserName = "yetir",
+    Password = "asdf1234"  ,
+    Port = 5672,
+    VirtualHost = "/"
+};
+ 
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "Yetir.DistributionFinalize",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    string message = "Hello World!";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: "",
+                         routingKey: "Yetir.DistributionFinalize",
+                         basicProperties: null,
+                         body: body);
+    Console.WriteLine(" [x] Sent {0}", message);
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
